1739ed831767a88d3a92c381c2bcc0f07123217a
Phili23 <rmariacarmen78@gmail.com>
2022-02-07 15:28:28 -0500

    detail succesfull

--- PI-Food-main/client/src/components/Detail/index.jsx
+++ PI-Food-main/client/src/components/Detail/index.jsx
@@ -24,13 +24,13 @@ export default function Detail(props) {
        <div > 
            <Link to='/home'><button >Back to main Page </button> </Link>
            <h1 > {detailsstate[0].title} </h1>
-           <img csrc={detailsstate[0].img ? detailsstate[0].img :'https://st.depositphotos.com/1036708/2191/i/600/depositphotos_21918797-stock-photo-knife-and-fork-with-plate.jpg'}/>
-           <h3 >Type Diet: {detailsstate[0].typeDiets.map(t => t.name)}</h3>
+           <img src={detailsstate[0].image ? detailsstate[0].image :'https://st.depositphotos.com/1036708/2191/i/600/depositphotos_21918797-stock-photo-knife-and-fork-with-plate.jpg'}/>
+           <h3 >Type Diet: {detailsstate[0].typeDiets.map(t => t.name).join( ' , ')}</h3>
            {/* <h4 >Dish Type: {detailsstate[0].dishTypes ? detailsstate[0].dishTypes.map(d => d.name) :'dish type not found'  }</h4> */}
            <h5 >summary: {detailsstate[0].summary}</h5>
            <h5 >healthScore: {detailsstate[0].healthScore}</h5>
            <h5 >puntutation: {detailsstate[0].spoonacularScore}</h5>
-           <h5>steps:{ Array.isArray(detailsstate[0].analyzedInstructions) ? detailsstate[0].analyzedInstructions.map(e => e.steps.map(f => f.step)) : detailsstate[0].analyzedInstructions }</h5>
+           <h5>steps:  { Array.isArray(detailsstate[0].steps) ? detailsstate[0].steps.map(e => e.steps.map(f => f.step)) : detailsstate[0].steps }</h5>
        </div> : 
        
        <div> <h2> loading... </h2> </div>
